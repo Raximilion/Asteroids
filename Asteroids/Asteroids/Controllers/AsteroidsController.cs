3cc9835e3b4b68bc1742c25e3624b4e507317d2c
raximilion <raximilion@gmail.com>
2023-04-03 17:32:03 +0200

    create the project

---
+++ Asteroids/Asteroids/Controllers/AsteroidsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asteroids.Controllers
+{
+    public class AsteroidsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
